b3bf88f32e84ae8d1bb306d86dc7ac31afe0cc67
han_heesoo <juntu09@gmail.com>
2023-07-31 18:00:38 +0900

    Modified 이미지 파일 유무에 따른 ocr 버튼 활성화 완료

--- boards/views.py
+++ boards/views.py
@@ -53,73 +53,74 @@ class Boards(APIView) :
 
         return Response(serializer.errors)
 
-class BoardDetail(APIView) :
+
+class BoardDetail(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_object(self, pk) :
-        try :
+    def get_object(self, pk):
+        try:
             board = Board.objects.get(pk=pk)
             return board
-        except Board.DoesNotExist :
+        except Board.DoesNotExist:
             raise NotFound
 
-    def get(self, request, pk) :
-        # pk를 가져와서 -> 보드 한개 가져오기 
+    def get(self, request, pk):
         board = self.get_object(pk)
-        # 보드 인스턴스를 -> JSON 형변환
         serializer = BoardSerializer(board)
-        # Response 객체로 반환  
         return Response(serializer.data)
-        
-    def put(self, request, pk) :
+
+    def put(self, request, pk):
         board = self.get_object(pk)
 
-        if not board.author == request.user : 
+        if not board.author == request.user:
             raise PermissionDenied
 
         serializer = BoardSerializer(instance=board, data=request.data, partial=True)
 
-        if serializer.is_valid() :
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        
+
         return Response(serializer.errors)
 
-    def delete(self, request, pk) :
+    def delete(self, request, pk):
         board = self.get_object(pk)
 
-        if not board.author == request.user : 
+        if not board.author == request.user:
             raise PermissionDenied
 
         board.delete()
         return Response({})
-    
-# 모델 정의
-class Post(models.Model):
-    image_file = models.ImageField(upload_to='images/', null=True, blank=True)
-    ocr_result = models.TextField(null=True, blank=True)
 
-# img -> text 변환 함수
-def convert_image_to_text(lang, image_path):
-    try:
-        text = pytesseract.image_to_string(Image.open(image_path), lang=lang)
-        return text
-    except Exception as e:
-        return str(e)
+    def post(self, request, pk):
+        board = self.get_object(pk)
 
-# 첨부 이미지 확인 -> 있으면 ocr 처리 함수
-def process_post(post_id, langs):
-    # 게시글 찾기
-    post = Post.objects.get(id=post_id)
-    
-    # 게시글에 이미지 있는지 확인
-    if post.image_file:
-        # 언어팩 기반 텍스트 추출
-        text = ''
-        for lang in langs:
-            text += convert_image_to_text(lang, post.image_file.path)
-            
-        # 텍스트 추출 결과가 빈 문자열이 아니면 -> JSON 형태 결과 저장
-        if text:
-            post.ocr_result = json.dumps({'data': text})
-            post.save()
+        # 이미지 파일 업로드 처리
+        if 'image' in request.FILES:
+            image_file = request.FILES['image']
+            if image_file.size > settings.FILE_SIZE_LIMIT:
+                raise ValidationError({'error': '이미지 파일 크기가 제한을 초과했습니다.'})
+
+            # 이미지 파일을 업로드하여 URL 획득
+            uploadcare = Uploadcare(public_key=settings.UC_PUBLIC_KEY, secret_key=settings.UC_SECRET_KEY)
+            ucare_file = uploadcare.upload(image_file)
+            image_url = f"https://ucarecdn.com/{ucare_file.uuid}/"
+            board.loaded_file = image_url
+
+            # 언어 설정
+            langs = request.data.getlist('lang')
+
+            # 텍스트 추출
+            text = ''
+            for lang in langs:
+                text += convert_image_to_text(lang, image_file)
+
+            # 텍스트가 빈 문자열이 아닌 경우 JSON 추출
+            if text:
+                board.ocr_result = json.dumps({'data': text})
+
+            board.save()
+            serializer = BoardSerializer(board)
+            return Response(serializer.data)
+
+        return Response({'error': '이미지 파일을 찾을 수 없습니다.'})
--- static/script/board-api.js
+++ static/script/board-api.js
@@ -5,11 +5,60 @@ $.ajax({
     "method": "GET",
     "timeout": 0,
 }).done(function (board) {
-    console.log(board);
+    console.log(board);                 // 확인용
     $('#author').text(board.author === null ? 'anonymous' : board.author.username);
     $('#title').val(board.title);
     $('#contents').val(board.contents);
     $('#loaded_file').attr('src', board.loaded_file);
     $('#created_at').val(board.created_at);
     $('#modified_at').val(board.modified_at);
+
+    // 이미지 파일 있으면 텍스트 변환 버튼 보여주기
+    if (board.loaded_file) {
+        $('#ocr-btn').show();
+    } else {
+        $('#ocr-btn').hide();
+    }
 });
+
+// 언어 팩 선택
+function selectLanguages() {
+    // checkbox에서 선택된 값 가져오기
+    const selectedLanguages = Array.from(document.querySelectorAll('input[name="language"]:checked')).map((checkbox) => checkbox.value);
+    return selectedLanguages;
+}
+
+// 텍스트 변환 버튼을 클릭했을 때 실행되는 함수
+function clickOcrBtn() {
+    const selectedLanguages = selectLanguages();
+    const image_file = document.getElementById("image-file").files[0];
+
+    if (selectedLanguages.length < 1) {
+        alert("적어도 하나 이상의 언어 팩을 선택해야 합니다.");
+        return;
+    }
+
+    const data = new FormData();
+    data.append('image', image_file);
+    data.append('lang', selectedLanguages);
+
+    $.ajax({
+        url: `/api/v1/boards/board/${board_number}`,
+        method: "POST",
+        data: data,
+        processData: false,
+        contentType: false,
+    })
+    .done((data) => {
+        if (data.data) {
+            // 반환된 텍스트가 있는 경우, 해당 텍스트를 화면에 표시 -> 파일로 바꾸기 
+            const ocr_result = data.data;
+            document.getElementById("ocr-result").innerText = ocr_result;
+        } else {
+            alert("텍스트 변환에 실패했습니다.");
+        }
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+}
